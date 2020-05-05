c8375a4f64ecc046580e7755da7670e8c9e1a6ac
Thomas Hunziker <werbemails@bakual.ch>
2020-05-05 08:04:12 +0200

    Get rid of "src" folder and "build" script

--- build/build.bat
+++
@@ -1,9 +0,0 @@
-REM This will generate the zipfiles for SermonSpeaker in /build/packages
-REM This needs the zip binaries from Info-Zip installed. An installer can be found http://gnuwin32.sourceforge.net/packages/zip.htm
-setlocal
-SET PATH=%PATH%;C:\Program Files (x86)\GnuWin32\bin
-rmdir /q /s packages
-mkdir packages
-REM Zip it
-cd ../src/
-zip -r ../build/packages/mod_herrnhuter_losungen.zip *
